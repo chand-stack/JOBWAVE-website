0b0a7b2c85f9c2dcf9929074a981d3818c45b2b4
chand-stack <chand.stack@gmail.com>
2023-11-06 04:11:02 +0600

    Chore: job cards added in home page

--- src/Components/Shared/JobsCard.jsx
+++ src/Components/Shared/JobsCard.jsx
@@ -1,7 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import useAxios from '../../hook/useAxios';
+import img from '../../assets/icons8-user.gif'
+import { FaPeopleGroup } from 'react-icons/fa6';
+import { useState } from 'react';
+
 
 const JobsCard = () => {
+
+    const [category,setCategory] = useState('')
+    console.log(category);
+
+    const axios = useAxios()
+    const {data: jobs, isLoading} = useQuery({
+        queryKey: ["jobs",category],
+        queryFn: async () => {
+            const res = await axios.get(`/all-job?category=${category}`);
+            return res
+        }
+    })
+
+
+
+    console.log(jobs);
     return (
         <div className="container mx-auto my-20 space-y-10">
             <h1 className="text-3xl md:text-4xl font-black text-white text-center">Newest <span className="text-[#A582F7]">Jobs</span> for You</h1>
@@ -10,27 +32,172 @@ const JobsCard = () => {
 <div className='mx-auto'>
 <Tabs className="  text-white">
     <TabList className=" mx-auto max-w-lg">
-      <Tab>All Recent</Tab>
-      <Tab>On Site Job</Tab>
-      <Tab>Remote Job</Tab>
-      <Tab>Hybrid</Tab>
-      <Tab>Part Time</Tab>
+      <Tab onClick={() => setCategory("")}>All Recent</Tab>
+      <Tab onClick={() => setCategory("onsite")}>On Site Job</Tab>
+      <Tab onClick={() => setCategory("remote")}>Remote Job</Tab>
+      <Tab onClick={() => setCategory("part-time")}>Hybrid</Tab>
+      <Tab onClick={() => setCategory("hybrid")}>Part Time</Tab>
     </TabList>
 
     <TabPanel>
-      <h2 className='mt-3'>Any content 1</h2>
+      {
+        isLoading? <div className='flex justify-center'>
+            <span className="loading loading-spinner text-info loading-lg mx-auto h-[50vh] text-center"></span>
+        </div> :
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 px-3'>
+
+        {
+            jobs?.data?.map(job => <div key={job._id} className='space-y-3 bg-[#111] p-3 rounded-lg'>
+              <div className='flex items-center gap-3'>
+              <img className='rounded-full' src={img} alt="" />
+              <div>
+              <h1 className='text-xl font-semibold'>{job.userName}</h1>
+                <p>Posted on {new Date(job.postingDate).toLocaleString()}</p>
+              </div>
+              </div>
+              <h1 className='text-2xl md:text-3xl font-semibold'>{job.title}</h1>
+              <p><span className='text-xl font-semibold text-[#A582F7]'>Application Deadline: </span> <span className='text-lg'>{new Date(job.deadline).toLocaleDateString()}</span></p>
+              <p className='text-xl'><span className='text-[#A582F7]'>Salary Range:</span> ${job.salary}k  per year</p>
+              <div className='flex justify-between'>
+                <button className='btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white'>View Details</button>
+                <div className='flex items-center gap-2'>
+                    <FaPeopleGroup className='text-2xl'/><p>{job.applicants} Applied</p>
+                </div>
+              </div>
+
+            </div>)
+        }
+
+      </div>
+      }
     </TabPanel>
     <TabPanel>
-      <h2>Any content 2</h2>
+    {
+        isLoading? <div className='flex justify-center'>
+            <span className="loading loading-spinner text-info loading-lg mx-auto h-[50vh] text-center"></span>
+        </div> :
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 px-3'>
+
+        {
+            jobs?.data?.map(job => <div key={job._id} className='space-y-3 bg-[#111] p-3 rounded-lg'>
+              <div className='flex items-center gap-3'>
+              <img className='rounded-full' src={img} alt="" />
+              <div>
+              <h1 className='text-xl font-semibold'>{job.userName}</h1>
+                <p>Posted on {new Date(job.postingDate).toLocaleString()}</p>
+              </div>
+              </div>
+              <h1 className='text-2xl md:text-3xl font-semibold'>{job.title}</h1>
+              <p><span className='text-xl font-semibold text-[#A582F7]'>Application Deadline: </span> <span className='text-lg'>{new Date(job.deadline).toLocaleDateString()}</span></p>
+              <p className='text-xl'><span className='text-[#A582F7]'>Salary Range:</span> ${job.salary}k  per year</p>
+              <div className='flex justify-between'>
+                <button className='btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white'>View Details</button>
+                <div className='flex items-center gap-2'>
+                    <FaPeopleGroup className='text-2xl'/><p>{job.applicants} Applied</p>
+                </div>
+              </div>
+
+            </div>)
+        }
+
+      </div>
+      }
     </TabPanel>
     <TabPanel>
-      <h2>Any content 3</h2>
+    {
+        isLoading? <div className='flex justify-center'>
+            <span className="loading loading-spinner text-info loading-lg mx-auto h-[50vh] text-center"></span>
+        </div> :
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 px-3'>
+
+        {
+            jobs?.data?.map(job => <div key={job._id} className='space-y-3 bg-[#111] p-3 rounded-lg'>
+              <div className='flex items-center gap-3'>
+              <img className='rounded-full' src={img} alt="" />
+              <div>
+              <h1 className='text-xl font-semibold'>{job.userName}</h1>
+                <p>Posted on {new Date(job.postingDate).toLocaleString()}</p>
+              </div>
+              </div>
+              <h1 className='text-2xl md:text-3xl font-semibold'>{job.title}</h1>
+              <p><span className='text-xl font-semibold text-[#A582F7]'>Application Deadline: </span> <span className='text-lg'>{new Date(job.deadline).toLocaleDateString()}</span></p>
+              <p className='text-xl'><span className='text-[#A582F7]'>Salary Range:</span> ${job.salary}k  per year</p>
+              <div className='flex justify-between'>
+                <button className='btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white'>View Details</button>
+                <div className='flex items-center gap-2'>
+                    <FaPeopleGroup className='text-2xl'/><p>{job.applicants} Applied</p>
+                </div>
+              </div>
+
+            </div>)
+        }
+
+      </div>
+      }
     </TabPanel>
     <TabPanel>
-      <h2>Any content 4</h2>
+    {
+        isLoading? <div className='flex justify-center'>
+            <span className="loading loading-spinner text-info loading-lg mx-auto h-[50vh] text-center"></span>
+        </div> :
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 px-3'>
+
+        {
+            jobs?.data?.map(job => <div key={job._id} className='space-y-3 bg-[#111] p-3 rounded-lg'>
+              <div className='flex items-center gap-3'>
+              <img className='rounded-full' src={img} alt="" />
+              <div>
+              <h1 className='text-xl font-semibold'>{job.userName}</h1>
+                <p>Posted on {new Date(job.postingDate).toLocaleString()}</p>
+              </div>
+              </div>
+              <h1 className='text-2xl md:text-3xl font-semibold'>{job.title}</h1>
+              <p><span className='text-xl font-semibold text-[#A582F7]'>Application Deadline: </span> <span className='text-lg'>{new Date(job.deadline).toLocaleDateString()}</span></p>
+              <p className='text-xl'><span className='text-[#A582F7]'>Salary Range:</span> ${job.salary}k  per year</p>
+              <div className='flex justify-between'>
+                <button className='btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white'>View Details</button>
+                <div className='flex items-center gap-2'>
+                    <FaPeopleGroup className='text-2xl'/><p>{job.applicants} Applied</p>
+                </div>
+              </div>
+
+            </div>)
+        }
+
+      </div>
+      }
     </TabPanel>
     <TabPanel>
-      <h2>Any content 5</h2>
+    {
+        isLoading? <div className='flex justify-center'>
+            <span className="loading loading-spinner text-info loading-lg mx-auto h-[50vh] text-center"></span>
+        </div> :
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-5 px-3'>
+
+        {
+            jobs?.data?.map(job => <div key={job._id} className='space-y-3 bg-[#111] p-3 rounded-lg'>
+              <div className='flex items-center gap-3'>
+              <img className='rounded-full' src={img} alt="" />
+              <div>
+              <h1 className='text-xl font-semibold'>{job.userName}</h1>
+                <p>Posted on {new Date(job.postingDate).toLocaleString()}</p>
+              </div>
+              </div>
+              <h1 className='text-2xl md:text-3xl font-semibold'>{job.title}</h1>
+              <p><span className='text-xl font-semibold text-[#A582F7]'>Application Deadline: </span> <span className='text-lg'>{new Date(job.deadline).toLocaleDateString()}</span></p>
+              <p className='text-xl'><span className='text-[#A582F7]'>Salary Range:</span> ${job.salary}k  per year</p>
+              <div className='flex justify-between'>
+                <button className='btn bg-gradient-to-t from-[#7367F0] from-10% via-[#A582F7] via-30% to-[#CE9FFC] to-90% border-none text-white'>View Details</button>
+                <div className='flex items-center gap-2'>
+                    <FaPeopleGroup className='text-2xl'/><p>{job.applicants} Applied</p>
+                </div>
+              </div>
+
+            </div>)
+        }
+
+      </div>
+      }
     </TabPanel>
   </Tabs>
 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import routes from './Routes/Routes.jsx'
 import AuthProvider from './AuthProvider/AuthProvider.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <AuthProvider><RouterProvider router={routes}></RouterProvider></AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
