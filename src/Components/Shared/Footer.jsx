32ca40592b2e88a4f2e502f9364064c4ef83de6b
chand-stack <chand.stack@gmail.com>
2023-11-08 17:55:15 +0600

    Chore: footer section updated

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -1,31 +1,46 @@
 import logo from "../../assets/Frame.svg";
 import { AiOutlineInstagram, AiFillTwitterCircle } from "react-icons/ai";
 import { LiaFacebook } from "react-icons/lia";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="container mx-auto ">
-      <footer className="footer footer-center p-10 text-primary-content">
+      <footer className="footer p-10 bg-black mt-10 py-20 text-white">
         <aside>
-          <div className="flex items-center">
+          <div className="flex items-center gap-2">
             <img src={logo} alt="" />
             <h1 className="text-2xl font-semibold text-white">JobWave</h1>
           </div>
-          <p className="font-bold">
+          <p>
             JobWave -<br />
             Navigating Your Career Future. Your path to success starts here.
           </p>
         </aside>
         <nav>
+          <header className=" text-2xl font-semibold text-white">
+            Find Us On Social Media
+          </header>
           <div className="grid grid-flow-col gap-4">
-            <div className="text-white text-center">
-              <p className="text-2xl font-semibold py-3">
-                Find Us On Social Media:
-              </p>
-              <div className="text-white flex items-center gap-4 text-5xl justify-center">
-                <LiaFacebook />
-                <AiOutlineInstagram />
-                <AiFillTwitterCircle />
-              </div>
+            <div className="text-white flex items-center gap-4 text-4xl justify-center">
+              <LiaFacebook />
+              <AiOutlineInstagram />
+              <AiFillTwitterCircle />
+            </div>
+          </div>
+        </nav>
+        <nav>
+          <header className=" text-2xl font-semibold text-gray-200">
+            COMPANY
+          </header>
+          <div className="grid grid-flow-col gap-4">
+            <div className="text-white items-center gap-4 text-xl justify-center">
+              <Link to="/blog">Blog</Link>
+              <br />
+              <Link to="/alljobs">All Job</Link>
+              <br />
+              <Link to="/login">Login</Link>
+              <br />
+              <Link to="/register">Register</Link>
             </div>
           </div>
         </nav>
